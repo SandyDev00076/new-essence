2a4479e58b5588ff0d76817720d4f885593ae513
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2025-02-23 16:46:32 +0530

    increase font size of content

--- src/components/DataSection.tsx
+++ src/components/DataSection.tsx
@@ -7,7 +7,7 @@ export default function DataSection({ title, content }: Props) {
   return (
     <section className=" mb-8">
       <h2 className="text-3xl text-green-400 font-semibold mb-2">{title}</h2>
-      <p className="text-white">{content}</p>
+      <p className="text-white text-lg">{content}</p>
     </section>
   );
 }
